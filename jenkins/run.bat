5f26c96ffcf6abf81037ce0376b5472cb777ee88
Kevin Phillips <thefriendlycoder@gmail.com>
2013-12-03 15:44:17 -0400

    Renamed jenkins war file back to original naming convention.
    Former-commit-id: 19c989b5e94db1609716977268e294b0eaccee33

--- jenkins/run.bat
+++ jenkins/run.bat
@@ -1,6 +1,6 @@
 @setlocal
 @echo off
-set JENKINS_HOME_FOLDER=jenkins1.541home
-set JENKINS_WAR_FILE=jenkins1.541.war
+set JENKINS_HOME=%CD%/.jenkins
+set JENKINS_WAR_FILE=jenkins.war
 
-java -DJENKINS_HOME=%CD%/%JENKINS_HOME_FOLDER% -jar %JENKINS_WAR_FILE%
+java -DJENKINS_HOME="%JENKINS_HOME%" -jar %JENKINS_WAR_FILE%
--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 #war file to launch
-JENKINS_WAR_FILE=jenkins1.541.war
+JENKINS_WAR_FILE=jenkins.war
 
-#redirect the Jenkins home folder to a sub-folder
-#under the current working directory named the same
-#name as the war file and with a 'home' suffix
-#eg: ./jenkins1.541home
-JENKINS_HOME=${JENKINS_WAR_FILE%.*}home
+#Home folder which should be redirected to a local sub-folder
+JENKINS_HOME=.jenkins
 
 #Sanity Check: Make sure the specified war file exists
 if [ ! -f "$JENKINS_WAR_FILE" ]
   then
 	echo "Jenkins war file not found: $JENKINS_WAR_FILE"
 	exit 10
-fi
-
-#Make sure the Jenkins home folder exists
-if [ ! -d "$JENKINS_HOME" ]; then
-   mkdir "$JENKINS_HOME"
 fi
 
 #Now launch Jenkins
