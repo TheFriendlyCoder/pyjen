d12ae2f7ac5fdf6b1c4a41e61c4141fc928761db
Kevin Phillips <thefriendlycoder@gmail.com>
2013-12-05 16:30:40 -0400

    Refactored regression test scripts to make them more modular and clean up some of the initial scripts added in the last commit.
    Former-commit-id: 807e90dd6615a45d6b0f02035a1869b0ac6be84e

--- tests/regression.py
+++ tests/regression.py
@@ -1,39 +1,19 @@
 import unittest
-import subprocess
-import shlex
-import datetime
+from test_utils import start_jenkins
 
 class regression_tests(unittest.TestCase):
-    __path_to_jenkins="../jenkins/jenkins.war"
+    
     __jenkins_process = None
+    
     def setUp(self):
-        
-        cmd_line = "java -jar " + self.__path_to_jenkins
-        args = shlex.split(cmd_line)
-        self.__jenkins_process = subprocess.Popen( args , stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-        #Wait until the Jenkins service has completed its startup
-        jenkinsRunning = False
-        while True:
-            #TODO: probably should stream this output to a log file somewhere for debugging
-            stderrdata = self.__jenkins_process.stderr.readline()
-            if stderrdata.find("Jenkins is fully up and running") >= 0:
-                jenkinsRunning = True
-                break
-    
-        self.assertTrue(jenkinsRunning, "Failed to launch jenkins instance")
+        self.__jenkins_process = start_jenkins()
          
     def tearDown(self):
         self.__jenkins_process.terminate()
         
     def test1(self):
-        print "In test one"
+        pass
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(regression_tests)
-    unittest.TextTestRunner(verbosity=3).run(suite)
-    
-    
-    print stderrdata
-    p.terminate()
-        
+    unittest.TextTestRunner(verbosity=3).run(suite)    
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from os import path
+import subprocess
+import shlex
+from os import path, environ
 
 def get_sample_data_file(filename):
     """Given a filename, this method generates the full path to the file in the sample data folder
@@ -57,3 +59,49 @@ def get_test_data_file(filename):
     assert(path.exists(retval))
     
     return retval
+
+def start_jenkins():
+    """Attempts to launch an isolated instance of Jenkins for testing purposes
+    
+    This function will block until the instance of Jenkins is successfully
+    loaded and ready for use by subsequent test logic
+    
+    NOTE: The host OS must have the JAVA_HOME environment variable set and it
+    must point to a supported version of java under which Jenkins will be run. 
+    
+    Returns
+    -------
+    subprocess
+        reference to the subprocess object used to launch Jenkins in a parallel
+        process so as to not block the subsequent test execution.
+        
+        when this instance of Jenkins is no longer needed simply call the
+        terminate() method on the returned object to close the app.
+    """ 
+    #Get the java installation folder from the environment
+    if not 'JAVA_HOME' in environ:
+        raise "JAVA_HOME environment variable not found"
+    java_home = environ['JAVA_HOME']
+    
+    #Generate a command line for running Jenkins in an isolated environment
+    path_to_jenkins="../jenkins/jenkins.war"
+    cmd_line = '"' + java_home + '/java.exe" -jar ' + path_to_jenkins
+    args = shlex.split(cmd_line)
+    
+    #Now launch Jenkins in a secondary, non-blocking process
+    jenkins_process = subprocess.Popen( args , stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    
+    #Wait until the Jenkins service has completed its startup before continuing
+    jenkinsRunning = False
+    while True:
+        #TODO: probably should stream this output to a log file somewhere for debugging
+        stderrdata = jenkins_process.stderr.readline()
+        if stderrdata.find("Jenkins is fully up and running") >= 0:
+            jenkinsRunning = True
+            break
+
+    if not jenkinsRunning:
+        raise "Failed to launch Jenkins instance: " + path_to_jenkins
+    
+    return jenkins_process
