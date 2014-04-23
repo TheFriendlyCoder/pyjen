c6b787b521e630bf402e42275fa0b3f09af960e4
Kevin Phillips <thefriendlycoder@gmail.com>
2014-04-23 00:28:35 -0300

    further improvements to error handling in virtualenv config script
    Former-commit-id: 3134b03be55c241473e4a4dd522536f7ee244d6c

--- venv.sh
+++ venv.sh
@@ -6,14 +6,22 @@ if [ $? -ne 0 ]
 fi
 
 pip freeze | grep virtualenv > /dev/null
-if [ $? -ne 0 ] 
-	then pip install virtualenv
+if [ $? -ne 0 ]; then 
+	pip install virtualenv
+	if [ $? -ne 0]; then 
+		echo "Error installing virtualenv dependency"
+		exit 1;
+	fi
 fi
 
 if [ ! -d py2 ]; then
 	virtualenv --no-site-packages -p python2 py2;
 	source py2/bin/activate;
 	pip install requests;
+	if [ $? -ne 0 ]; then
+		echo "Error installing requests dependency in virtualenv 2.x"
+		exit 1;
+	fi
 	deactivate;
 fi
 
@@ -21,5 +29,9 @@ if [ ! -d py3 ]; then
 	virtualenv --no-site-packages -p python3 py3;
 	source py3/bin/activate;
 	pip install requests;
+	if [ $? -ne 0 ]; then
+		echo "Error installing requests dependency in virtualenv 3.x"
+		exit 1;
+	fi
 	deactivate;
 fi
