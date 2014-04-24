1725c1eba67416a476acb5fa30b75401829e716f
Kevin Phillips <thefriendlycoder@gmail.com>
2014-04-23 22:04:40 -0300

    Added missing packages to wheel file
    Former-commit-id: 24d4b85d64f2664a802bdbc6c1faff6750cf7ae8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Kevin S. Phillips',
     author_email='kevin@thefriendlycoder.com',
     license='LICENSE.txt',
-    packages=['pyjen'],
+    packages=['pyjen', 'pyjen.utils', 'pyjen.plugins'],
     description='Python wrapper for the Jenkins CI REST API',
     long_description=open('README.md').read(),
     url='https://github.com/TheFriendlyCoder/pyjen',
