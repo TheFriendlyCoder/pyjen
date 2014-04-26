d5831dd06888c142bcdc3f818ee573397ac53335
Kevin Phillips <thefriendlycoder@gmail.com>
2014-04-26 15:33:01 -0300

    created helper script to download latest jenkins versions for testing

---
+++ functional_tests/jenkins/get_latest_wars.sh
@@ -0,0 +1,7 @@
+if [ ! -f jenkins_latest.war ]; then
+	wget -O jenkins_latest.war http://mirrors.jenkins-ci.org/war/latest/jenkins.war;
+fi
+
+if [ ! -f jenkins_lts.war ]; then
+	wget -O jenkins_lts.war http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war;
+fi
