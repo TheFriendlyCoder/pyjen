318e3d92a160863b7ae5e47c4fbcfdb1568ff4ff
Kevin Phillips <thefriendlycoder@gmail.com>
2014-06-25 00:21:58 -0300

    added script for pushing packages to pypi

--- run.sh
+++ run.sh
@@ -64,6 +64,9 @@ publish()
 
 	# Publish API docs to thefriendlycoder.com
 	ncftpput -R -v -m pyjentfc /PyJen ./docs/build/html/*
+
+	# Publish package to PyPi
+	python setup.py sdist upload	
 }
 
 show_usage()
