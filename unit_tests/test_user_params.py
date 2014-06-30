2a796fc487b45369853fabc55b41400d002ab3fd
Kevin Phillips <thefriendlycoder@gmail.com>
2014-06-30 00:16:40 -0300

    Fixed platform specific errors with Python 2

--- unit_tests/test_user_params.py
+++ unit_tests/test_user_params.py
@@ -5,6 +5,12 @@ import pytest
 import os
 import platform
 
+import sys
+if sys.version_info.major < 3:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+    
 class JenkisConfigParser_tests(unittest.TestCase):
     def test_get_default_configfiles(self):
         default_config_files = JenkinsConfigParser.get_default_configfiles()
@@ -28,20 +34,28 @@ class JenkisConfigParser_tests(unittest.TestCase):
             
     def test_get_credentials_empty_configuration(self):
         test_obj = JenkinsConfigParser()
-        sample_config=""
-        test_obj.read_string(sample_config)
+        sample_config=StringIO("")
+        test_obj.readfp(sample_config)
         
         self.assertEqual(test_obj.get_credentials("http://localhost:8080"), None, 
                          "Empty config files should provide empty credentials")
+    
+    def test_empty_section(self):
+        test_url = "http://localhost:8080"
+        sample_config=StringIO("[http://localhost:8080]")
+        test_obj = JenkinsConfigParser()
+        test_obj.readfp(sample_config)
+        
+        self.assertEqual(test_obj.get_credentials(test_url), None, 
+                         "Undefined credentials should be reported as empty")
         
     def test_get_credentials_anonymous(self):
         test_url = "http://localhost:8080"
-        sample_config="""[http://localhost:8080]
-        username=
-        password=
-        """
+        sample_config=StringIO("[http://localhost:8080]\n" +
+        "username=\n" +
+        "password=\n")
         test_obj = JenkinsConfigParser()
-        test_obj.read_string(sample_config)
+        test_obj.readfp(sample_config)
         
         self.assertEqual(test_obj.get_credentials(test_url), None, 
                          "Config explicitly declares empty username and password which should \
@@ -51,46 +65,34 @@ class JenkisConfigParser_tests(unittest.TestCase):
         test_url = "http://localhost:8080"
         expected_username = "jdoe"
         expected_password = "Password123"
-        
-        sample_config="""[http://localhost:8080]
-        username=jdoe
-        password=Password123
-        """
+                
+        sample_config=StringIO("[http://localhost:8080]\n" +
+                               "username=jdoe\n" +
+                               "password=Password123\n")
         test_obj = JenkinsConfigParser()
-        test_obj.read_string(sample_config)
+        test_obj.readfp(sample_config)
         
         actual_credentials = test_obj.get_credentials(test_url)
         self.assertEqual(actual_credentials[0], expected_username)
         self.assertEqual(actual_credentials[1], expected_password)
         
-    def test_empty_section(self):
-        test_url = "http://localhost:8080"
-        sample_config="[http://localhost:8080]"
-        test_obj = JenkinsConfigParser()
-        test_obj.read_string(sample_config)
-        
-        self.assertEqual(test_obj.get_credentials(test_url), None, 
-                         "Undefined credentials should be reported as empty")
-        
     def test_get_credentials_no_username(self):
         test_url = "http://localhost:8080"
         
-        sample_config="""[http://localhost:8080]
-        password=Password123
-        """
+        sample_config=StringIO("[http://localhost:8080]\n" +
+        "password=Password123\n")
         test_obj = JenkinsConfigParser()
-        test_obj.read_string(sample_config)
+        test_obj.readfp(sample_config)
         
         self.assertRaises(InvalidUserParamsError, test_obj.get_credentials, test_url)
         
     def test_get_credentials_no_password(self):
         test_url = "http://localhost:8080"
         
-        sample_config="""[http://localhost:8080]
-        username=jdoe
-        """
+        sample_config=StringIO("[http://localhost:8080]\n" +
+        "username=jdoe\n")
         test_obj = JenkinsConfigParser()
-        test_obj.read_string(sample_config)
+        test_obj.readfp(sample_config)
         
         self.assertRaises(InvalidUserParamsError, test_obj.get_credentials, test_url)
         
