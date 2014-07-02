3c84c79ac50b8bac75c521040d632b47e6c3cf23
Kevin Phillips <thefriendlycoder@gmail.com>
2014-07-02 19:26:05 -0300

    Updated version number for next release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyjen',
-    version='0.0.5dev',
+    version='0.0.6dev',
     author='Kevin S. Phillips',
     author_email='kevin@thefriendlycoder.com',
     license=open('LICENSE.txt').read(),
