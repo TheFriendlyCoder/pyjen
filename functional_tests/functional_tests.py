66d6a39298f5d7e67f37bc07e08996a8c1ec287c
Kevin Phillips <thefriendlycoder@gmail.com>
2014-07-03 23:00:40 -0300

    fixed functional tests for pyjen api

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -1,56 +1,52 @@
 import unittest
-from functional_tests.test_utils import start_jenkins
+import pytest
+from .test_utils import start_jenkins 
 from pyjen.jenkins import Jenkins
 from pyjen.view import View
 import os
 import shutil
 
-class regression_tests(unittest.TestCase):
+class empty_jenkins_regression_tests(unittest.TestCase):
+    """Tests that require a clean, unfettered Jenkins instance"""
     
-    #Reference to the subprocess object that manages the Jenkins
-    #instance which runs concurrently with these tests
-    __jenkins_process = None
-    
-    #URL of main dashboard for the local Jenkins instance
-    __jenkins_url = "http://localhost:8080"
-       
     @classmethod
     def setUpClass(self):
         print ("Preparing Jenkins Instance...")
-        self.__jenkins_home = os.path.abspath("./jenkins_home")
-        #TODO: Detect whether jenkins is already running at the default URL and prompt user if we should just use that one
-        self.__jenkins_process = start_jenkins(self.__jenkins_home)
+        self._jenkins_home = os.path.abspath("./empty_jenkins_home")
+        os.makedirs(self._jenkins_home)
+        
+        self._jenkins_process = start_jenkins("./jenkins/jenkins_lts.war", self._jenkins_home)
+        
+        self._jenkins_url = "http://localhost:8080"
          
     @classmethod
     def tearDownClass(self):
-        #shut Jenkins down
-        if self.__jenkins_process != None:
-            self.__jenkins_process.terminate()
+        if self._jenkins_process != None:
+            self._jenkins_process.terminate()
             
         #clean up working folder
-        if os.path.exists(self.__jenkins_home):
-            shutil.rmtree(self.__jenkins_home)
+        if os.path.exists(self._jenkins_home):
+            shutil.rmtree(self._jenkins_home)
     
     def test_create_view(self):
         new_view_name = "test_view"
-        j = Jenkins(self.__jenkins_url)
+        j = Jenkins.easy_connect(self._jenkins_url, None)
         j.create_view(new_view_name, View.LIST_VIEW)
         v = j.find_view(new_view_name)
         
         self.assertTrue(v != None)
         
-        self.assertEqual(self.__jenkins_url + "/view/" + new_view_name + "/", v.get_url())
+        self.assertEqual(v.name, new_view_name)
         
     def test_shutdown(self):
-        j = Jenkins(self.__jenkins_url)
+        j = Jenkins.easy_connect(self._jenkins_url, None)
 
-        self.assertFalse(j.is_shutting_down())
+        self.assertFalse(j.is_shutting_down)
         j.prepare_shutdown()
-        self.assertTrue(j.is_shutting_down())
+        self.assertTrue(j.is_shutting_down)
         
         j.cancel_shutdown()
-        self.assertFalse(j.is_shutting_down())
+        self.assertFalse(j.is_shutting_down)
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(regression_tests)
-    unittest.TextTestRunner(verbosity=3).run(suite)    
+    pytest.main()
--- functional_tests/test_utils.py
+++ functional_tests/test_utils.py
@@ -1,20 +1,18 @@
+"""Helper functions used by the pyjen functional tests"""
 import subprocess
 import shlex
 import os
 import locale
 
-def start_jenkins(home_folder=None):
+def start_jenkins(war_file, home_folder):
     """Attempts to launch an isolated instance of Jenkins for testing purposes
     
     This function will block until the instance of Jenkins is successfully
     loaded and ready for use by subsequent test logic
     
-    NOTE: The host OS must have the JAVA_HOME environment variable set and it
-    must point to a supported version of java under which Jenkins will be run. 
-    
-    Returns
-    -------
-    subprocess
+    :param str war_file: path to the Jenkins war file to be launched
+    :param str home_folder: path to the folder to be used as the Jenkins home folder
+    :returns:
         reference to the subprocess object used to launch Jenkins in a parallel
         process so as to not block the subsequent test execution.
         
@@ -23,7 +21,7 @@ def start_jenkins(home_folder=None):
     """ 
     #see if we have a JAVA_HOME env var set
     java_home = os.getenv('JAVA_HOME')
-    
+
     #Generate a command line for running Jenkins in an isolated environment
     if java_home:
         java_app = os.path.join(java_home, "bin", "java")
@@ -32,33 +30,27 @@ def start_jenkins(home_folder=None):
         #can be located on the global system path
         java_app = "java"
         
-    path_to_jenkins="../jenkins/jenkins.war"
-    
-    java_options=""
-    if home_folder:
-        java_options=' -DJENKINS_HOME="' + home_folder + '"'
+    java_options=' -DJENKINS_HOME="' + home_folder + '"'
 
-    cmd_line = '"' + java_app + '"' + java_options + ' -jar ' + path_to_jenkins
+    cmd_line = '"' + java_app + '"' + java_options + ' -jar ' + war_file
     args = shlex.split(cmd_line)
     
     #Now launch Jenkins in a secondary, non-blocking process
     jenkins_process = subprocess.Popen( args , stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     
     #Wait until the Jenkins service has completed its startup before continuing
-    jenkinsRunning = False
+    jenkins_running = False
     
     encoding = locale.getdefaultlocale()[1]
     while True:
-        #TODO: probably should stream this output to a log file somewhere for debugging
         stderrdata = jenkins_process.stderr.readline()
         tmp = stderrdata.decode(encoding)
         if tmp.find("Jenkins is fully up and running") >= 0:
-            jenkinsRunning = True
+            jenkins_running = True
             break
 
-    if not jenkinsRunning:
-        raise "Failed to launch Jenkins instance: " + path_to_jenkins
+    if not jenkins_running:
+        raise "Failed to launch Jenkins instance: " + war_file
     
     return jenkins_process
 
