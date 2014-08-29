39a2856a1c644d4db039887cbb9f02e236834e35
Kevin Phillips <thefriendlycoder@gmail.com>
2014-08-28 22:22:02 -0300

    minor cleanup to virtualenv config script

--- venv.bat
+++ venv.bat
@@ -1,5 +1,5 @@
 @echo off
-virtualenv -p 3 .\py3 > venv.log 2>&1
-call .\py3\scripts\activate.bat >> venv.log 2>&1
-pip install requests wheel sphinx pylint pytest pytest-cov mock >> venv.log 2>&1
-echo "Python virtual environment successfully configured. Run 'deactivate' to restore environment.
+if not exist logs mkdir logs
+if not exist py3 virtualenv -p 3 .\py3 > .\logs\venv.log 2>&1
+call .\py3\scripts\activate.bat >> .\logs\venv.log 2>&1
+@echo Python virtual environment successfully configured. Run 'deactivate' to restore environment.
