f63c937eb851412c47042060193452a4948b9d6f
Kevin Phillips <thefriendlycoder@gmail.com>
2014-10-05 23:00:40 -0300

    Fixed error reporting in Windows virtualenv scripts.

--- venv2.bat
+++ venv2.bat
@@ -1,5 +1,14 @@
 @echo off
 if not exist logs mkdir logs
 if not exist py2 virtualenv -p 2 .\py2 > .\logs\venv.log 2>&1
+if errorlevel 1 (
+	@echo Failed to launch virtualenv
+	exit /B 1
+)
+
 call .\py2\scripts\activate.bat >> .\logs\venv.log 2>&1
+if errorlevel 1 (
+	@echo Failed to activate virtualenv
+	exit /B 1
+)
 @echo Python virtual environment successfully configured. Run 'deactivate' to restore environment.
