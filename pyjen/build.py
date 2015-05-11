6aa30065560c1d1afb1ef0c17740a606d84d54b5
unknown <jdyer@jdyerws.caris.priv>
2015-05-11 14:35:52 -0300

    Fixed spacing for def result

--- pyjen/build.py
+++ pyjen/build.py
@@ -95,7 +95,7 @@ class Build(object):
         return self._data_io.get_text("/consoleText")
 
     @property
-        def result(self):
+    def result(self):
         """Gets the final status of this build
 
         :return: the status of this build. Typically "SUCCESS" or "FAILURE" but may also be "UNSTABLE"
