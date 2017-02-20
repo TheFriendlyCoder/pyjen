dad47501516f93d413ad9f2082e51f10d1b95e86
Kevin Phillips <thefriendlycoder@gmail.com>
2017-02-19 21:36:14 -0400

    updated helper script for docker

--- launch.sh
+++ launch.sh
@@ -1,2 +1,4 @@
-[ -d jenkins_home ] || mkdir jenkins_home
+#!/bin/bash -ex
+[ -d jenkins_home ] || (mkdir jenkins_home && sudo chown 1000 jenkins_home)
 docker run -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_home:/var/jenkins_home jenkins
+#docker run --rm -i -t -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_home:/var/jenkins_home jenkins /bin/bash
