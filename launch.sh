0facbdcabac5e52974ba9603f7c9bb68ca578229
Kevin Phillips <kevinp@ca.ibm.com>
2017-02-23 14:37:34 -0400

    fixed bugs with jenkins launcher script

--- launch.sh
+++ launch.sh
@@ -1,23 +1,27 @@
 #!/bin/bash -e
 export version=$1
 
-export V1IMAGE="jenkins:1.651.3"
-export V2IMAGE="jenkins:latest"
+export V1IMAGE="jenkins:1.651.3-alpine"
+export V2IMAGE="jenkins:alpine"
 export V1HOME=jenkins1_home
 export V2HOME=jenkins2_home
 
 if [ "$version" == "1" ]
 then
-    export IMAGE=$V1IMAGE
-    export HOME=$V1HOME
+    echo Running Jenkins 1 container...
+    export JKIMAGE=$V1IMAGE
+    export JKHOME=$V1HOME
+    export JKPORT1=8081
+    export JKPORT2=50001
 else
-    export IMAGE=$V2IMAGE
-    export HOME=$V2HOME
+    echo Running Jenkins 2 conatiner...
+    export JKIMAGE=$V2IMAGE
+    export JKHOME=$V2HOME
+    export JKPORT1=8080
+    export JKPORT2=50000
 fi
 
-echo Home is $HOME
-echo Image is $IMAGE
-exit
-
-[ -d jenkins_home ] || (mkdir jenkins_home && sudo chown 1000 jenkins_home)
-docker run -p 8080:8080 -p 50000:50000 -v $PWD/jenkins_home:/var/jenkins2_home $V1IMAGE
+[ -d $JKHOME ] || (mkdir $JKHOME && chmod a+w $JKHOME)
+docker run -p $JKPORT1:8080 -p $JKPORT2:50000 -v $PWD/$JKHOME:/var/jenkins_home $JKIMAGE
+#docker run -i -t -p 8080:8080 -p 50000:50000 -v $PWD/$JKHOME:/var/delme $JKIMAGE /bin/bash
+#docker run -p 8080:8080 -p 50000:50000 $JKIMAGE
