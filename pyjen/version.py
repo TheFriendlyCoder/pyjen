d2b1cd75656eb27f62f4feadc519a56eca78dc4e
Kevin Phillips <kevinp@ca.ibm.com>
2017-05-11 23:37:10 -0300

    Prototype for new version handling logic

--- pyjen/version.py
+++ pyjen/version.py
@@ -1,8 +1,4 @@
-"""PyJen package version"""
+"""package version"""
+import pkg_resources
 
-# NOTE: We declare the version number here, separately from the other PyJen modules, so that it can be parsed without
-# having to import any PyJen modules. This is helpful when using the setup.py script to generate redistributable
-# packages as it is safer to not import any modules from the modules you are packaging.  (ie: importing a module may
-# fail if the import depends on other modules being installed, which may or may not exist on the system generating
-# the package)
-__version__ = "1.0.0"
+__version__ = pkg_resources.get_distribution("pyjen").version  # pylint: disable=no-member
