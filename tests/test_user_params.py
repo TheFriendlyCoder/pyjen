6123164b093a41eae03beda9fd92f3a783df3e9c
Kevin Phillips <thefriendlycoder@gmail.com>
2019-03-04 20:43:39 -0400

    Fixes #30 Removed dependencies on unittest framework

--- tests/test_user_params.py
+++ tests/test_user_params.py
@@ -1,101 +1,103 @@
 from pyjen.utils.user_params import JenkinsConfigParser
 from pyjen.exceptions import InvalidUserParamsError
-import unittest
 import pytest
 import os
 import platform
+from six import StringIO
 
-import sys
-if sys.version_info.major < 3:
-    from StringIO import StringIO
-else:
-    from io import StringIO
-    
-class JenkisConfigParser_tests(unittest.TestCase):
-    def test_get_default_configfiles(self):
-        default_config_files = JenkinsConfigParser.get_default_configfiles()
-        
-        # Currently we look in 2 locations for config files
-        self.assertEqual(len(default_config_files), 2)
-        
-        # The least privileged location should be the users home folder
-        self.assertTrue(default_config_files[0].startswith(os.path.join(os.path.expanduser("~"))))
-        
-        # the next most privileged location is the local working folder
-        self.assertTrue(default_config_files[1].startswith(os.getcwd()))
-        
-        # In any case, each path must point to the expected config file name
-        if platform.system() == "Windows":
-            expected_filename = "pyjen.cfg"
-        else:
-            expected_filename = ".pyjen"
-        for filename in default_config_files:
-            self.assertTrue(filename.endswith(expected_filename))
-            
-    def test_get_credentials_empty_configuration(self):
-        test_obj = JenkinsConfigParser()
-        sample_config=StringIO("")
-        test_obj.readfp(sample_config)
-        
-        self.assertEqual(test_obj.get_credentials("http://localhost:8080"), None, 
-                         "Empty config files should provide empty credentials")
-    
-    def test_empty_section(self):
-        test_url = "http://localhost:8080"
-        sample_config=StringIO("[http://localhost:8080]")
-        test_obj = JenkinsConfigParser()
-        test_obj.readfp(sample_config)
-        
-        self.assertEqual(test_obj.get_credentials(test_url), None, 
-                         "Undefined credentials should be reported as empty")
-        
-    def test_get_credentials_anonymous(self):
-        test_url = "http://localhost:8080"
-        sample_config=StringIO("[http://localhost:8080]\n" +
-        "username=\n" +
-        "password=\n")
-        test_obj = JenkinsConfigParser()
-        test_obj.readfp(sample_config)
-        
-        self.assertEqual(test_obj.get_credentials(test_url), None, 
-                         "Config explicitly declares empty username and password which should \
-                         be reported as empty credentials")
 
-    def test_get_credentials(self):
-        test_url = "http://localhost:8080"
-        expected_username = "jdoe"
-        expected_password = "Password123"
-                
-        sample_config=StringIO("[http://localhost:8080]\n" +
-                               "username=jdoe\n" +
-                               "password=Password123\n")
-        test_obj = JenkinsConfigParser()
-        test_obj.readfp(sample_config)
-        
-        actual_credentials = test_obj.get_credentials(test_url)
-        self.assertEqual(actual_credentials[0], expected_username)
-        self.assertEqual(actual_credentials[1], expected_password)
-        
-    def test_get_credentials_no_username(self):
-        test_url = "http://localhost:8080"
-        
-        sample_config=StringIO("[http://localhost:8080]\n" +
-        "password=Password123\n")
-        test_obj = JenkinsConfigParser()
-        test_obj.readfp(sample_config)
-        
-        self.assertRaises(InvalidUserParamsError, test_obj.get_credentials, test_url)
-        
-    def test_get_credentials_no_password(self):
-        test_url = "http://localhost:8080"
-        
-        sample_config=StringIO("[http://localhost:8080]\n" +
-        "username=jdoe\n")
-        test_obj = JenkinsConfigParser()
-        test_obj.readfp(sample_config)
-        
-        self.assertRaises(InvalidUserParamsError, test_obj.get_credentials, test_url)
-        
-    
+def test_get_default_configfiles():
+    default_config_files = JenkinsConfigParser.get_default_configfiles()
+
+    # Currently we look in 2 locations for config files
+    assert len(default_config_files) == 2
+
+    # The least privileged location should be the users home folder
+    assert default_config_files[0].startswith(os.path.join(os.path.expanduser("~")))
+
+    # the next most privileged location is the local working folder
+    assert default_config_files[1].startswith(os.getcwd())
+
+    # In any case, each path must point to the expected config file name
+    if platform.system() == "Windows":
+        expected_filename = "pyjen.cfg"
+    else:
+        expected_filename = ".pyjen"
+    for filename in default_config_files:
+        assert filename.endswith(expected_filename)
+
+
+def test_get_credentials_empty_configuration():
+    test_obj = JenkinsConfigParser()
+    sample_config=StringIO("")
+    test_obj.readfp(sample_config)
+
+    assert test_obj.get_credentials("http://localhost:8080") is None
+
+
+def test_empty_section():
+    test_url = "http://localhost:8080"
+    sample_config=StringIO("[http://localhost:8080]")
+    test_obj = JenkinsConfigParser()
+    test_obj.readfp(sample_config)
+
+    assert test_obj.get_credentials(test_url) is None
+
+
+def test_get_credentials_anonymous():
+    test_url = "http://localhost:8080"
+    sample_config=StringIO("""[http://localhost:8080]
+username=
+password=
+""")
+    test_obj = JenkinsConfigParser()
+    test_obj.readfp(sample_config)
+
+    assert test_obj.get_credentials(test_url) is None
+
+
+def test_get_credentials():
+    test_url = "http://localhost:8080"
+    expected_username = "jdoe"
+    expected_password = "Password123"
+
+    sample_config=StringIO("""[http://localhost:8080]
+username=jdoe
+password=Password123
+""")
+    test_obj = JenkinsConfigParser()
+    test_obj.readfp(sample_config)
+
+    actual_credentials = test_obj.get_credentials(test_url)
+    assert actual_credentials[0] == expected_username
+    assert actual_credentials[1] == expected_password
+
+
+def test_get_credentials_no_username():
+    test_url = "http://localhost:8080"
+
+    sample_config=StringIO("""[http://localhost:8080]
+password=Password123
+""")
+    test_obj = JenkinsConfigParser()
+    test_obj.readfp(sample_config)
+
+    with pytest.raises(InvalidUserParamsError):
+        test_obj.get_credentials(test_url)
+
+
+def test_get_credentials_no_password():
+    test_url = "http://localhost:8080"
+
+    sample_config=StringIO("""[http://localhost:8080]
+username=jdoe
+""")
+    test_obj = JenkinsConfigParser()
+    test_obj.readfp(sample_config)
+
+    with pytest.raises(InvalidUserParamsError):
+        test_obj.get_credentials(test_url)
+
+
 if __name__ == "__main__":
     pytest.main([__file__, "-v", "-s"])
