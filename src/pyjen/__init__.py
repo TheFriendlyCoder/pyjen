7b7476c564a86750a7683513d3046d595cc5f4f3
Kevin Phillips <thefriendlycoder@gmail.com>
2019-05-12 19:15:48 -0300

    Fixes #185 Fixed version declaration

--- src/pyjen/__init__.py
+++ src/pyjen/__init__.py
@@ -3,11 +3,6 @@ Abstraction layer for the Jenkins REST API designed to simplify the interaction
 with the Jenkins web interface from the Python scripting environment.
 """
 import logging
-import ast
-import pkg_resources
-
-__version__ = ast.literal_eval(
-    pkg_resources.resource_string(__name__, "version.prop").decode("utf-8")
-)
+from .version import __version__
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
