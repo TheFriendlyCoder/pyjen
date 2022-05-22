a2ac416eab6f408f05bfa4835c662ce3b1269c2c
Kevin Phillips <thefriendlycoder@gmail.com>
2022-05-21 23:50:28 -0300

    Close #276 Disabled Docker tests when unneded

--- tests/test_changeset.py
+++ tests/test_changeset.py
@@ -1,5 +1,30 @@
 import pytest
+from .utils import async_assert
+from pyjen.plugins.gitscm import GitSCM
+from pyjen.plugins.freestylejob import FreestyleJob
 
+
+@pytest.mark.vcr()
+@pytest.fixture(scope="class")
+def test_builds_with_git(request, jenkins_api):
+    """Helper fixture that creates a job with a sample build with Git sources for testing"""
+    request.cls.jenkins = jenkins_api
+    request.cls.job = jenkins_api.create_job(request.cls.__name__ + "Job", FreestyleJob)
+    assert request.cls.job is not None
+
+    expected_url = "https://github.com/TheFriendlyCoder/pyjen.git"
+    test_scm = GitSCM.instantiate(expected_url)
+    request.cls.job.scm = test_scm
+
+    request.cls.job.quiet_period = 0
+    async_assert(lambda: isinstance(request.cls.job.scm, GitSCM))
+
+    request.cls.job.start_build()
+    async_assert(lambda: request.cls.job.last_good_build is not None)
+
+    yield
+
+    request.cls.job.delete()
 
 @pytest.mark.usefixtures('test_builds_with_git')
 class TestBuildsWithGit(object):
